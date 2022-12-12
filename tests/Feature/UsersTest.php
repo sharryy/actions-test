d6436561a37156846563163976f397f0d0d0675b
Shehryar Amin <58107398+sharryy@users.noreply.github.com>
2022-12-12 12:32:40 +0500

    CI(testing-workflow): Create Laravel.yml
    CI(testing-workflow): replace SQLite with MySQL
    
    CI(testing-workflow): replace SQLite with MySQL
    
    CI: Update Laravel.yml
    
    ci: Update Laravel.yml
    
    CI: Update Laravel.yaml
    
    CI: Update Laravel.yml
    
    CI: Update Laravel.yml
    
    CI: Update Laravel.yml
    
    wip
    
    wip
    
    wip
    
    wip
    
    wip
    
    wip
    
    wip
    
    wip
    
    wip
    
    wip
    
    wip
    
    wip
    
    wip
    
    wip
    
    wip
    
    wip
    
    wip
    
    wip
    
    wip
    
    wip
    
    wip

--- tests/Feature/UsersTest.php
+++ tests/Feature/UsersTest.php
@@ -3,52 +3,39 @@
 namespace Tests\Feature;
 
 use App\Models\User;
-use Illuminate\Foundation\Testing\RefreshDatabase;
-use Illuminate\Foundation\Testing\WithFaker;
-use Tests\TestCase;
 
-class UsersTest extends TestCase
-{
-    use RefreshDatabase;
+it('can create a user', function () {
+    $this->assertDatabaseCount('users', 0);
 
-    /** @test */
-    public function it_can_create_a_user()
-    {
-        $this->assertDatabaseCount('users', 0);
+    $response = $this->post(route('user.store'), [
+        'name'     => 'John Doe',
+        'email'    => 'john@gmail.com',
+        'password' => 'password',
+    ])->assertOk();
 
-        $response = $this->post(route('user.store'), [
-            'name'     => 'John Doe',
-            'email'    => 'john@gmail.com',
-            'password' => 'password',
-        ])->assertOk();
+    $this->assertDatabaseCount('users', 1);
 
-        $this->assertDatabaseCount('users', 1);
+    $this->assertDatabaseHas('users', [
+        'name'  => 'John Doe',
+        'email' => 'john@gmail.com',
+    ]);
+});
 
-        $this->assertDatabaseHas('users', [
-            'name'  => 'John Doe',
-            'email' => 'john@gmail.com',
-        ]);
-    }
+it('can update a user', function () {
+    $user = User::factory()->create();
 
-    /** @test */
-    public function it_can_update_a_user()
-    {
-        $user = User::factory()->create();
+    $this->assertDatabaseCount('users', 1);
 
-        $this->assertDatabaseCount('users', 1);
+    $response = $this->post(route('user.update', $user), [
+        'name'     => 'John Doe',
+        'email'    => 'changed@gmail.com',
+        'password' => 'password',
+    ])->assertOk();
 
-        $response = $this->post(route('user.update', $user), [
-            'name'     => 'John Doe',
-            'email'    => 'changed@gmail.com',
-            'password' => 'password',
-        ])->assertOk();
+    $this->assertDatabaseCount('users', 1);
 
-        $this->assertDatabaseCount('users', 1);
-
-        $this->assertDatabaseHas('users', [
-            'name'  => 'John Doe',
-            'email' => 'changed@gmail.com',
-        ]);
-
-    }
-}
+    $this->assertDatabaseHas('users', [
+        'name'  => 'John Doe',
+        'email' => 'changed@gmail.com',
+    ]);
+});
